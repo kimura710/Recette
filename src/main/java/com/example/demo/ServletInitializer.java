c02e1955bd1b87927366dcf0e05557b570e285aa
kimura <pankata79.th@gmail.com>
2020-12-14 06:41:15 +0900

    ServletInitializerにmainクラス継承

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -8,6 +9,9 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Portfolio01Application.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio01Application.class, args);
 	}
 
 }
