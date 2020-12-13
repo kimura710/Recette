329b1b5a87de97c1cf2ce84568fdf6b36267c161
kimura <pankata79.th@gmail.com>
2020-12-14 06:12:42 +0900

    テーブル追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,3 +5,30 @@ course varchar(10),
 recette varchar(1000),
 memo varchar(1000)
 );
+
+CREATE TABLE IF NOT EXISTS m_user (
+  user_id VARCHAR(50) PRIMARY KEY,
+  password VARCHAR(255),
+  pass_update_date datetime,
+  login_miss_times INT,
+  account_unlock boolean,
+  tenant_id VARCHAR(50),
+  user_name VARCHAR(50),
+  mail_address VARCHAR(50),
+  enabled BOOLEAN,
+  user_due_date datetime
+);
+
+/* 権限マスタテーブル */
+CREATE TABLE IF NOT EXISTS m_role (
+  role_id VARCHAR(50) PRIMARY KEY,
+  role_name VARCHAR(100)
+);
+
+/* ユーザー権限テーブル */
+/* ユーザーと権限を紐付けるテーブル */
+CREATE TABLE IF NOT EXISTS t_user_role (
+  id INT PRIMARY KEY,
+  user_id VARCHAR(50),
+  role_id VARCHAR(50)
+);
